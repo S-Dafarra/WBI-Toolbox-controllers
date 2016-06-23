cfb65722cac66f81c110d844c2000ce72b71c633
Stefano <stefano.dafarra@gmail.com>
2016-06-23 11:44:01 +0200

    New version for capture point and fpe.

--- controllers/torqueBalancing/src/fpe_computation.m
+++ controllers/torqueBalancing/src/fpe_computation.m
@@ -1,14 +1,8 @@
-function [fpe, fpe_alternative, theta_bar] = fpe_computation(m, COMx, COM_vel, J_COM, n, g, COPx)
+function [fpe, fpe_alternative, theta_bar] = fpe_computation(m, COMx, COM_vel, J_COM, n, g, COPx, foot)
 % This function computes the residual of the foot placement estimator
 % formula
 
 theta_bar = 0;
-
-n_norm = norm(n,2);
-if (n_norm>0)
-    n = n/n_norm;
-else n = zeros(3,1);
-end
 
 h_dot = COM_vel(3); % vertical speed of the COM
 h = COMx(3);
@@ -24,6 +18,7 @@ vx = COM_vel(1:3)'*x;
 
 COM_proj = zeros(2,1);
 COP_proj = zeros(2,1);
+foot_proj = zeros(2,1);
 
 COM_proj(1) = COMx'*x;
 COM_proj(2) = COMx'*z;
@@ -31,8 +26,10 @@ COM_proj(2) = COMx'*z;
 COP_proj(1) = COPx'*x;
 COP_proj(2) = COPx'*z;
 
-l = norm(COM_proj-COP_proj,2);
-theta_dot = vx*(COM_proj(2)-COP_proj(2))/(l^2);
+foot_proj(1) = foot'*x;
+foot_proj(2) = foot'*z;
+
+theta_dot = vx*(h)/(h^2+(COM_proj(1)-foot_proj(1)));
 
 coder.extrinsic('solve_fpe')
 theta_bar = double(solve_fpe(theta_dot, m, h, vx, h_dot, J, g));
